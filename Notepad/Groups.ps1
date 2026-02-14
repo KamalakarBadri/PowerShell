2710ea27fcc7b2dd724a351d39ff545efeda5396
KamalakarBadri <74125291+KamalakarBadri@users.noreply.github.com>
2026-02-15 01:54:41 +0530

    Update Groups.ps1

--- Notepad/Groups.ps1
+++ Notepad/Groups.ps1
@@ -44,3 +44,53 @@ $csvData = foreach ($group in $allGroups) {
 
 $csvData | Export-Csv -Path "Groups.csv" -NoTypeInformation
 Write-Host "Exported $($allGroups.Count) groups to Groups.csv" -ForegroundColor Green
+
+
+
+
+# SIMPLIFIED VERSION
+$TenantId = "0e439a1f-a497-462b-9e6b-4e582e203607"
+$ClientId = "73efa35d-6188-42d4-b258-838a977eb149"
+$ClientSecret = "nxH8Q~cnM2fkLf75~klgzsH9rzOC~~OgY.DEGaz"
+$TenantName = "geekbyteonline.onmicrosoft.com"
+
+# Get current date and time for filename
+$currentDateTime = Get-Date -Format "yyyyMMdd_HHmmss"
+$currentDateForReport = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+# Get Microsoft Graph token
+Write-Host "Getting tokens..." -ForegroundColor Yellow
+
+
+Write-Host "Graph token: OK" -ForegroundColor Green
+
+# SharePoint token
+$Certificate = Get-Item Cert:\CurrentUser\My\B799789F78628CAE56B4D0F380FD551EB754E0DB
+$hash = [Convert]::ToBase64String($Certificate.GetCertHash())
+$exp = [math]::Round((Get-Date).AddMinutes(10).ToFileTimeUtc()/10000000 - 11644473600)
+$nbf = [math]::Round((Get-Date).ToFileTimeUtc()/10000000 - 11644473600)
+
+$header = @{alg="RS256"; typ="JWT"; x5t=$hash -replace '\+','-' -replace '/','_' -replace '='}
+$payload = @{aud="https://login.microsoftonline.com/$TenantName/oauth2/token"; exp=$exp; iss=$ClientId; jti=[guid]::NewGuid(); nbf=$nbf; sub=$ClientId}
+
+$encodedHeader = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes(($header | ConvertTo-Json))) -replace '\+','-' -replace '/','_' -replace '='
+$encodedPayload = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes(($payload | ConvertTo-Json))) -replace '\+','-' -replace '/','_' -replace '='
+
+$unsignedJWT = "$encodedHeader.$encodedPayload"
+$privateKey = ([System.Security.Cryptography.X509Certificates.RSACertificateExtensions]::GetRSAPrivateKey($Certificate))
+$signature = [Convert]::ToBase64String($privateKey.SignData([Text.Encoding]::UTF8.GetBytes($unsignedJWT), [Security.Cryptography.HashAlgorithmName]::SHA256, [Security.Cryptography.RSASignaturePadding]::Pkcs1)) -replace '\+','-' -replace '/','_' -replace '='
+
+$JWT = "$unsignedJWT.$signature"
+
+$sharepointToken = (Invoke-RestMethod -Uri "https://login.microsoftonline.com/$TenantName/oauth2/v2.0/token" -Method Post -Body @{
+    client_id=$ClientId; client_assertion=$JWT
+    client_assertion_type="urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
+    scope="https://geekbyteonline.sharepoint.com/.default"; grant_type="client_credentials"
+} -ContentType "application/x-www-form-urlencoded").access_token
+Write-Host "SharePoint token: OK" -ForegroundColor Green
+
+$graphToken = (Invoke-RestMethod -Uri "https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token" -Method Post -Body @{
+    client_id=$ClientId; client_assertion=$JWT
+    client_assertion_type="urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
+    scope="https://graph.microsoft.com/.default"; grant_type="client_credentials"
+} -ContentType "application/x-www-form-urlencoded").access_token
